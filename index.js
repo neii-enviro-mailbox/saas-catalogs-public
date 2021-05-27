3e10b581309ef45fd79e151174387009eb45932a
Nick Forbes-Smith <nick.forbes-smith@data61.csiro.au>
2021-05-27 14:10:56 +1000

    Test
    Test

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
+const mapConfigs = [
+  { id: "map-config-de-australia", dev: "de-australia/map-config/dev.json" },
+];
+
 try {
   const addedFiles = (process.env.ADDED_FILES ?? "").split(",") ?? [];
   const changedFiles = (process.env.CHANGED_FILES ?? "").split(",") ?? [];
 
   console.log(addedFiles);
   console.log(changedFiles);
+
+  console.log(github.context);
+
+  // mapConfigs.forEach((map) => {});
 
   // X-Magda-API-Key-Id & X-Magda-API-Key headers
 
