e2875ac91eac80bb5fadd24f4b91242bf6251177
Nick Forbes-Smith <nick.forbes-smith@data61.csiro.au>
2021-05-27 15:41:34 +1000

    refactor

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const fsPromises = require("fs/promises");
 const fetch = require("node-fetch");
-
-const mapConfigs = [
-  { id: "map-config-de-australia", file: "de-australia/map-config/dev.json" },
-];
+const mapConfigs = require("./map-configs.json");
 
 async function readFile(path) {
   try {
@@ -18,6 +15,10 @@ async function readFile(path) {
 
 (async () => {
   try {
+    // envTag (dev, test or prod) is used to find corresponding map-config file
+    const envTag = process.env.ENVIRONMENT_TAG;
+    console.log(`Udating files in **${envTag}** environment`);
+
     const addedFiles = (process.env.ADDED_FILES ?? "").split(",") ?? [];
     const modifiedFiles = (process.env.MODIFIED_FILES ?? "").split(",") ?? [];
 
@@ -28,24 +29,29 @@ async function readFile(path) {
 
     for (let i = 0; i < mapConfigs.length; i++) {
       const mapConfig = mapConfigs[i];
-      if (changedFiles.includes(mapConfig.file)) {
-        const devJson = (await readFile(mapConfig.file)).toString();
+
+      const mapConfigFile = mapConfig[envTag];
+      const mapConfigId = mapConfig.id;
+
+      // Has a map-config file changed for the currentMapConfig?
+      if (changedFiles.includes(mapConfigFile)) {
+        const devJson = (await readFile(mapConfigFile)).toString();
         console.log(
-          `Updating \`${mapConfig.id}\` from path \`${mapConfig.file}\` (in ${process.env.DEV_MAGDA_FQDN})`
+          `Updating \`${mapConfigId}\` from path \`${mapConfigFile}\` (in ${process.env.MAGDA_FQDN})`
         );
-        const response = await fetch(
-          `https://${process.env.DEV_MAGDA_FQDN}/api/v0/registry-auth/records/${mapConfig.id}`,
+        await fetch(
+          `https://${process.env.MAGDA_FQDN}/api/v0/registry-auth/records/${mapConfigId}`,
           {
             headers: {
-              "X-Magda-API-Key-Id": process.env.DEV_MAGDA_API_ID,
-              "X-Magda-API-Key": process.env.DEV_MAGDA_API_KEY,
+              "X-Magda-API-Key-Id": process.env.MAGDA_API_ID,
+              "X-Magda-API-Key": process.env.MAGDA_API_KEY,
               "Content-Type": "application/json",
             },
             body: devJson,
             method: "PUT",
           }
         );
-        console.log(`SUCCESSFULLY updated \`${mapConfig.id}\``);
+        console.log(`SUCCESSFULLY updated \`${mapConfigId}\``);
       }
     }
   } catch (error) {
