668f0bb67d85b1bd2ef5a77d37769b7df009f4a8
Nick Forbes-Smith <nick.forbes-smith@data61.csiro.au>
2021-07-01 14:38:40 +1000

    refine diff-maps script

--- diff-maps.js
+++ diff-maps.js
@@ -2,6 +2,7 @@ const jsonDiff = require("json-diff");
 const mapConfigs = require("./map-configs.json");
 
 const fsPromises = require("fs/promises");
+const colors = require("colors");
 
 async function readFile(path) {
   try {
@@ -12,7 +13,39 @@ async function readFile(path) {
   }
 }
 
+function removeCommonChangedProps(obj) {
+  if (obj?.aspects?.["terria-config"]?.initializationUrls) {
+    delete obj.aspects["terria-config"].initializationUrls;
+  }
+
+  if (obj?.aspects?.["terria-config"]?.parameters?.globalDisclaimer__deleted) {
+    delete obj.aspects["terria-config"].parameters.globalDisclaimer__deleted;
+  }
+
+  if (obj?.aspects?.["terria-config"]?.parameters?.googleAnalyticsKey__added) {
+    delete obj.aspects["terria-config"].parameters.googleAnalyticsKey__added;
+  }
+
+  if (
+    obj?.aspects?.["terria-config"]?.parameters &&
+    Object.keys(obj.aspects["terria-config"].parameters).length === 0
+  ) {
+    delete obj.aspects["terria-config"];
+  }
+
+  if (
+    obj?.aspects?.["terria-config"] &&
+    Object.keys(obj.aspects["terria-config"]).length === 0
+  ) {
+    delete obj.aspects["terria-config"];
+  }
+  if (obj?.aspects && Object.keys(obj.aspects).length === 0) {
+    delete obj.aspects;
+  }
+}
+
 (async () => {
+  console.log(`\n\n MAP CONFIG DIFF CHECKER\n\n`.bgWhite.yellow.bold);
   for (let i = 0; i < mapConfigs.length; i++) {
     const mapConfig = mapConfigs[i];
     const mapConfigFiles = await Promise.all(
@@ -28,11 +61,40 @@ async function readFile(path) {
       })
     );
 
+    console.log(`CHECKING MAP CONFIG ${mapConfig.id}\n`);
+
     const devToTest = jsonDiff.diff(mapConfigFiles[0], mapConfigFiles[1]);
-    console.log(devToTest);
+    removeCommonChangedProps(devToTest);
     if (devToTest && Object.keys(devToTest).length > 0) {
-      console.log(`CHANGED map-config ${mapConfig.id}`);
+      console.log(`DIFF DEV -> TEST: ${mapConfig.id}`.blue.bold);
+      console.log(
+        "____________________________________________________________________________________________________\n"
+      );
+      console.log(
+        jsonDiff.diffString(mapConfigFiles[0], mapConfigFiles[1], {
+          color: true,
+        })
+      );
+      console.log(
+        "____________________________________________________________________________________________________\n"
+      );
     }
-    console.log();
+
+    const testToProd = jsonDiff.diff(mapConfigFiles[1], mapConfigFiles[2]);
+    removeCommonChangedProps(testToProd);
+    if (testToProd && Object.keys(testToProd).length > 0) {
+      console.log(`DIFF TEST -> PROD: ${mapConfig.id}`.magenta.bold);
+      console.log(
+        "____________________________________________________________________________________________________\n"
+      );
+      console.log(
+        jsonDiff.diffString(mapConfigFiles[1], mapConfigFiles[2], {
+          color: true,
+        })
+      );
+      console.log(
+        "____________________________________________________________________________________________________\n"
+      );
+    }
   }
 })();
